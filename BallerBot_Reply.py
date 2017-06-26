f811731a858b4c015aeddfcde75cc6b4f54f2e20
davecrob2 <davecrob2@gmail.com>
2017-06-26 17:10:19 -0500

    remove nonfunctional code from tutorial

--- BallerBot_Reply.py
+++ BallerBot_Reply.py
@@ -7,8 +7,6 @@ Created on Wed Jun 21 23:32:01 2017
 
 import praw
 import pdb
-import re
-import os
 import csv
 
 #Variables assigned to the playerlist file stored on computer
@@ -23,31 +21,12 @@ with open(PlayersCsv, mode='r') as infile:
 #Create the Reddit instance
 reddit = praw.Reddit('ballerbot')
 
-#Creates an empty list or loads list of posts we have replied to and removes empty values
-
-if not os.path.isfile("posts_replied_to.txt"):
-	posts_replied_to = []
-else:
-	with open("posts_replied_to.txt", "r") as f:
-		posts_replied_to = f.read()
-		posts_replied_to = posts_replied_to.split("\n")
-		posts_replied_to = list(filter(None, posts_replied_to))
-
 #Get the top 5 hot posts from r/NBA
 subreddit = reddit.subreddit('pythonforengineers')
 for submission in subreddit.hot(limit = 5):
-	#If we haven't replied to this post
-	if submission.id not in posts_replied_to:
-		#Search for keywords in the specified field, in this case: Title
-		for key in PlayerDict:
-			if key in submission.title:#Reply to the post with text, eventually the statistics of the player
-			     submission.reply("""What is your favorite sport?|Basketball is my favorite sport 
+    for key in PlayerDict:
+        if key in submission.title:
+            submission.reply("""What is your favorite sport?|Basketball is my favorite sport 
                     :--:|:--
                     What is the best sport?|Basketball""")
-			     print("Bot replying to: ", submission.title)
-			#Store the current submission id into our list
-			     posts_replied_to.append(submission.id)
-#Write our updated list back to the file
-with open("posts_replied_to.txt","w") as f:
-	for post_id in posts_replied_to:
-		f.write(post_id + "\n")
+            print("Bot replying to: ", submission.title)
