e55ea80443d2089845bdec6cb4d92e96bcd3281d
davecrob2 <davecrob2@gmail.com>
2017-06-27 13:38:54 -0500

    Scraping the Basketball Reference page

---
+++ BR_Scraper.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 26 23:20:28 2017
+
+@author: davecrob2
+"""
+
+import requests
+
+from bs4 import BeautifulSoup
+
+#Variable to store basketball reference player glossary
+page = requests.get("http://www.basketball-reference.com/players/a/")
+
+#Using BS4 to parse page
+soup = BeautifulSoup(page.content,'html.parser')
+
+
+#Finds "tbody" tag which contains the enitre player table
+tbody=soup.find('tbody')
+#print(soup.find('tbody'))
+#print(tbody.prettify())
+
+#Finds "tr" tag in "tbody" tag
+tr=tbody.find('tr')
+
+#Finds "th" tag in "tr" tag with class "Left"
+th=tr.find('th',class_='left')
+
+#Finds "a" tag in "th" tag
+a=th.find('a')
+
+#Stores the player name
+name =a.get_text()
+#print(name)
+
+#Stores the link portion to the player page
+player_id=a['href']
+#print(player_id)    
+
+
+#Extracts and stores the unique Player_id from the player link
+id_extract=player_id[-14:-6]
+#print(id_extract)
+
+#PLayer dictionary where all player names and ids will eventually be stored
+player_dict={}
+player_dict[name]=id_extract
+#print(player_dict)
+
+
+#Test code to find all links. Needs to be modified to only find player links
+#for a in soup.find_all('a',href=True):
+#    print("Found the URL:",a['href'])
