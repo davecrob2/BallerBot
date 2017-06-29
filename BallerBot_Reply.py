12ad1ca92a63ab5086e7eb339fa9793cdb547266
davecrob2 <davecrob2@gmail.com>
2017-06-28 19:27:33 -0500

    combined BR_Scraper. Replies to comments using live player listing

--- BallerBot_Reply.py
+++ BallerBot_Reply.py
@@ -6,28 +6,43 @@ Created on Wed Jun 21 23:32:01 2017
 """
 
 import praw
-import pdb
-import csv
+from player_scraper import bb_scrape
+from string import ascii_lowercase
 
-#Variables assigned to the playerlist file stored on computer
-PlayersCsv="C:\\Users\\davecrob2\\Documents\\GitHub\\BallerBot\\PlayerList_A-C.csv"
-#Opens PlayersCSV and reads .csv file
-with open(PlayersCsv, mode='r') as infile:
-    reader = csv.reader(infile)
-    
-    #iterates through rows to turn the list into an accesible python dictionary
-    PlayerDict = {rows[0]:rows[1] for rows in reader}
+
 
 #Create the Reddit instance
 reddit = praw.Reddit('ballerbot')
 
-#Get the top 5 hot posts from r/NBA
+#Selects subreddit
 subreddit = reddit.subreddit('pythonforengineers')
+#List to store each basketball reference link
+link_list = []
 
+#Dictionary to store all player names and IDs
+all_players={}
+
+#
+for a in ascii_lowercase:
+    link = "http://www.basketball-reference.com/players/%(a)s/" % {"a":a}
+    link_list.append(link)
+#print(link_list)
+
+#For loop to update player listing from Basketball Reference website
+for link in link_list:
+    try:
+        all_players.update(bb_scrape(link))
+    except AttributeError:
+        continue
+        print("There are no names here")
+
+
+#Code for replying to posts
 for submission in subreddit.hot(limit = 10):
     for top_level_comment in submission.comments:
-        for key in PlayerDict:
+        for key in all_players:
             if key in top_level_comment.body:
-                top_level_comment.reply("It works!")
+                #if top_level_comment.id not in replied_to:
+                    top_level_comment.reply("It works!")
             #print("Bot replying to: ", submission.title)
 
